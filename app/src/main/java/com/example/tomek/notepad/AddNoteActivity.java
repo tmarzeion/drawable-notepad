86f8fdd31fa4308c8f3a9655bf7bbeb56d1ffffd
Tomasz Marzeion <szafir1100@gmail.com>
2016-01-17 19:10:28 +0100

    added new note Activity

---
+++ app/src/main/java/com/example/tomek/notepad/AddNoteActivity.java
@@ -0,0 +1,13 @@
+package com.example.tomek.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AddNoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+    }
+}
--- app/src/main/java/com/example/tomek/notepad/MainActivity.java
+++ app/src/main/java/com/example/tomek/notepad/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.tomek.notepad;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -22,8 +23,8 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this, AddNoteActivity.class);
+                startActivity(intent);
             }
         });
     }
