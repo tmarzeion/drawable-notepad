9065706c9eeb678d0c3d949d5dc58ee374aec221
Tomasz Marzeion <szafir1100@gmail.com>
2016-01-18 17:09:34 +0100

    added note activity toolbar

--- app/src/main/java/com/example/tomek/notepad/NoteActivity.java
+++ app/src/main/java/com/example/tomek/notepad/NoteActivity.java
@@ -2,6 +2,7 @@ package com.example.tomek.notepad;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class NoteActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class NoteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_note, menu);
+        return true;
     }
 }
