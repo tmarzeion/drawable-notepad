1f5d9ad8ede2c4aa2efbb1f04d83d9ebab860c47
Tomasz Marzeion <szafir1100@gmail.com>
2016-01-27 13:09:39 +0100

    simplified bottom menu mechanics, now its handled in NoteActivity class instead of separate one

--- app/src/main/java/com/example/tomek/notepad/NoteActivity.java
+++ app/src/main/java/com/example/tomek/notepad/NoteActivity.java
@@ -1,38 +1,52 @@
 package com.example.tomek.notepad;
 
+import android.content.Context;
+import android.graphics.Point;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-
-import com.example.tomek.notepad.FormatTextContainer;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 public class NoteActivity extends AppCompatActivity {
 
-    FormatTextContainer root;
+    private static final double MENU_MARGIN_RELATIVE_MODIFIER = 0.3;
+    private LinearLayout mSliderLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        this.root = (FormatTextContainer) this.getLayoutInflater().inflate(R.layout.activity_note, null);
-
-        this.setContentView(root);
+        setContentView(R.layout.activity_note);
+        mSliderLayout = (LinearLayout) findViewById(R.id.sliderMenu);
+        ViewGroup.LayoutParams params = mSliderLayout.getLayoutParams();
+        params.height = calculateMenuMargin();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_note, menu);
         return true;
     }
-/*
-    public void toggleMenu(View v){
-        this.root.toggleMenu();
-    }*/
+
 
     public void toggleMenu(MenuItem item) {
-        this.root.toggleMenu();
+        if (findViewById(R.id.sliderMenu).getVisibility() == View.VISIBLE) {
+            findViewById(R.id.sliderMenu).setVisibility(View.GONE);
+        } else {
+            findViewById(R.id.sliderMenu).setVisibility(View.VISIBLE);
+        }
+    }
+
+    private int calculateMenuMargin() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        Display display = wm.getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        int height = size.y;
+        return  ((int) Math.round(height * MENU_MARGIN_RELATIVE_MODIFIER));
     }
 }
