da5a21e5480687646523d36b6a1cfe828646502d
koroshiya <korocresent@gmail.com>
2016-10-12 10:25:52 +1100

    Replaced static UI elements with a callback mechanism. Also fixed a string formatting issue (it was working, but erroneous)

--- app/src/main/java/com/example/tomek/notepad/SaveOrUpdateNoteTask.java
+++ app/src/main/java/com/example/tomek/notepad/SaveOrUpdateNoteTask.java
@@ -1,16 +1,19 @@
 package com.example.tomek.notepad;
 
 import android.app.Activity;
+import android.app.Application;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
+public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> implements Application.ActivityLifecycleCallbacks {
 
     private final Activity mCallingActivity;
     private final DatabaseHandler mDbHandler;
     private final boolean mIsUpdating;
+    private Activity currentActivity = null;
 
 
     /**
@@ -23,6 +26,7 @@ public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
         mCallingActivity = callingActivity;
         mDbHandler = databaseHandler;
         mIsUpdating = isUpdating;
+        callingActivity.getApplication().registerActivityLifecycleCallbacks(this);
     }
 
     @Override
@@ -44,15 +48,47 @@ public class SaveOrUpdateNoteTask extends AsyncTask<Note, Void, Void> {
     @Override
     protected void onPostExecute(Void result) {
 
-        ArrayList<Note> allNotes = (ArrayList<Note>) mDbHandler.getAllNotesAsArrayList();
-
         if (mIsUpdating) {
             Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_updated), Toast.LENGTH_SHORT).show();
         } else {
             Toast.makeText(mCallingActivity, mCallingActivity.getString(R.string.toast_note_created), Toast.LENGTH_SHORT).show();
-            MainActivity.noteAdapter.add(allNotes.get(mDbHandler.getNoteCount() - 1));
         }
-        MainActivity.noteAdapter.setData(allNotes);
-        MainActivity.noteAdapter.notifyDataSetChanged();
+
+        updateNoteListView();
+
     }
+
+    private void updateNoteListView(){
+        if (currentActivity != null && currentActivity instanceof MainActivity){
+            ArrayList<Note> allNotes = mDbHandler.getAllNotesAsArrayList();
+            Note newNote = null;
+            if (!mIsUpdating){
+                newNote = allNotes.get(mDbHandler.getNoteCount() - 1);
+            }
+            ((MainActivity) currentActivity).setListViewData(allNotes, newNote);
+        }
+    }
+
+    @Override
+    public void onActivityCreated(Activity activity, Bundle savedInstanceState) {}
+
+    @Override
+    public void onActivityStarted(Activity activity) {}
+
+    @Override
+    public void onActivityResumed(Activity activity) {
+        currentActivity = activity;
+    }
+
+    @Override
+    public void onActivityPaused(Activity activity) {}
+
+    @Override
+    public void onActivityStopped(Activity activity) {}
+
+    @Override
+    public void onActivitySaveInstanceState(Activity activity, Bundle outState) {}
+
+    @Override
+    public void onActivityDestroyed(Activity activity) {}
 }
